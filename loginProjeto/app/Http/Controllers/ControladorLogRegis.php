cea87afab67567c0dab8a76516534501e36c9427
Isaac <zackhyou@gmail.com>
2024-09-07 20:21:21 -0300

    🥅 - Tratamento Controller

--- loginProjeto/app/Http/Controllers/ControladorLogRegis.php
+++ loginProjeto/app/Http/Controllers/ControladorLogRegis.php
@@ -26,6 +26,12 @@ class ControladorLogRegis extends Controller
 
         $credentials = $request->only('email', 'password');
 
+        if(Auth::attempt($credentials)){
+
+            return redirect()->intended(route('home'));
+        }
+
+        return redirect(route('login'))->with("error", "Algo estranho ocorreu");
     }
 
     function registrationPost(Request $request){
@@ -40,3 +46,13 @@ class ControladorLogRegis extends Controller
         $data['email'] = $request->email;
         $data['password'] = Hash::make($request->password);
         $user = User::create($data);
+
+        if(!$user){
+
+            return redirect(route('registration'))->with("error", "Registro falhou");
+        
+        }
+
+        return redirect(route('login'))->with("sucesso", "O registro foi um sucesso!");
+    
+    }
