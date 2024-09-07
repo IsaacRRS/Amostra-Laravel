307b811c6b49fdc51fa44d8f4002c1a8796820cf
Isaac <zackhyou@gmail.com>
2024-09-07 20:21:52 -0300

    🚧 - Routes | Regis + Login

--- loginProjeto/routes/web.php
+++ loginProjeto/routes/web.php
@@ -7,3 +7,9 @@ use App\Http\Controllers\ControladorLogRegis;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/login', [ControladorLogRegis::class, 'login'])->name('login');
+Route::post('/login', [ControladorLogRegis::class, 'loginPost'])->name('login.post');
+
+Route::get('/registration', [ControladorLogRegis::class, 'registration'])->name('registration');
+Route::post('/registration', [ControladorLogRegis::class, 'registrationPost'])->name('registration.post');
